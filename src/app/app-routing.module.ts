9fe926e1b614266e0f2d83ed4917cff52f6c4626
Dave Riedstra <dave.riedstra@gmail.com>
2019-01-21 11:52:08 -0500

    configure routing for new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewEntryComponent } from './new-entry/new-entry.component';
+import { StatsComponent } from './stats/stats.component';
+import { PreferencesComponent } from './preferences/preferences.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+}, {
+  path: 'new-entry',
+  component: NewEntryComponent
+}, {
+  path: 'stats',
+  component: StatsComponent
+}, {
+  path: 'preferences',
+  component: PreferencesComponent
+}, {
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'home'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
