c6c08a89b19c2b1bad5eb405512557d9b60a3cff
Dave Riedstra <dave.riedstra@gmail.com>
2019-01-28 11:36:50 -0500

    show most recent entry details on home

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { FakeAfstore } from '../../mocks/fake-afstore';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FakeAfAuth } from '../../mocks/fakeafauth';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let fakeAfstore: FakeAfstore;
+  let fakeAfAuth: FakeAfAuth;
 
   beforeEach(async(() => {
+    fakeAfstore = new FakeAfstore();
+    fakeAfAuth = new FakeAfAuth();
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: AngularFireAuth, useValue: fakeAfAuth },
+        { provide: AngularFirestore, useValue: fakeAfstore }
+      ]
     })
     .compileComponents();
   }));
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Entry } from '../models/entry.model';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { take, takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   lastEntry: Entry;
+  unsubscribe: Subject<void>;
 
-  constructor() { }
+  constructor(
+    private afstore: AngularFirestore,
+    private afAuth: AngularFireAuth) {
+  }
 
   ngOnInit() {
+    this.unsubscribe = new Subject();
+    this.afAuth.user.pipe(
+      take(1)
+    ).subscribe(u => {
+      const uid = u.uid;
+
+      this.afstore.doc(`entries/${uid}`)
+        .collection('entries', ref => ref.orderBy('date', 'desc').limit(1) )
+        .valueChanges()
+        .pipe(
+          takeUntil(this.unsubscribe)
+        )
+        .subscribe((e: Entry[]) => {
+          if (e instanceof Array) {
+            e[0].date = e[0].date.toDate();
+            this.lastEntry = e[0];
+          }
+        });
+    });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
