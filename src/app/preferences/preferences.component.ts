c6c9b5dc9dccbfeacaed003131845c706fc260d9
Dave Riedstra <dave.riedstra@gmail.com>
2019-01-29 12:48:03 -0500

    show current email on prefs page

--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-preferences',
   templateUrl: './preferences.component.html',
   styleUrls: ['./preferences.component.scss']
 })
-export class PreferencesComponent implements OnInit {
+export class PreferencesComponent implements OnInit, OnDestroy {
+  user: User;
+  private unsubscribe: Subject<void>;
 
   constructor(private router: Router, private auth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.unsubscribe = new Subject();
+    this.auth.user.pipe(
+      takeUntil(this.unsubscribe)
+    )
+    .subscribe(u => this.user = u);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   back(e: Event) {
