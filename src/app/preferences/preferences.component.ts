59e935a3388735de11d7d8960cb108df17bcdaeb
Dave Riedstra <dave.riedstra@gmail.com>
2019-01-29 13:44:53 -0500

    wire up template to component controller

--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { User } from 'firebase';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-preferences',
@@ -13,6 +14,13 @@ import { User } from 'firebase';
 export class PreferencesComponent implements OnInit, OnDestroy {
   user: User;
   private unsubscribe: Subject<void>;
+
+  // form models
+  newEmail = '';
+  retypeEmail = '';
+  currentPassword = '';
+  newPassword = '';
+  retypePassword = '';
 
   constructor(private router: Router, private auth: AngularFireAuth) { }
 
@@ -39,5 +47,35 @@ export class PreferencesComponent implements OnInit, OnDestroy {
       .then(() => {
         this.router.navigateByUrl('/');
       });
+  }
+
+  /**
+   * submit handler for the whole form.
+   * Determines whether to update email and/or password.
+   */
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    if (this.newEmail) {
+      this.updateEmail();
+    }
+
+    if (this.newPassword) {
+      this.updatePassword();
+    }
+  }
+
+  updateEmail() {
+    console.log('updating email to', this.newEmail);
+  }
+
+  updatePassword() {
+    console.log('updating password to', this.newPassword);
+  }
+
+  deleteAccountData() {
+    console.log('deleting account data!');
   }
 }
