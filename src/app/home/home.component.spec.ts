9eef7ee01c526a4aa5b8fd0dbe6650a05a1e4acb
Dave Riedstra <dave.riedstra@gmail.com>
2019-03-08 12:52:45 -0500

    get entries from most recent non-empty month

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -7,16 +7,48 @@ import { FakeAfAuth } from '../../mocks/fakeafauth';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { NewlinePipe } from '../newline.pipe';
 import { VisualizerComponent } from '../visualizer/visualizer.component';
+import { Entry } from '../models/entry.model';
+import { FakeAfstoreDoc } from '../../mocks/fake-afstore-doc';
+import { FakeAfstoreColl } from '../../mocks/fake-afstore-coll';
+
+const makeEntriesBetween = (afterDate: Date, beforeDate: Date) => {
+  const out: Entry[] = [];
+  const numEntries = Math.round(Math.random() * 7 + 2);
+  const startDate = afterDate.getUTCDate(),
+    dateRange = beforeDate.getUTCDate() - startDate;
+  for (let i = 0; i < numEntries; i++) {
+    const d = new Date(afterDate);
+    d.setDate(d.getDate() + (Math.random() * dateRange));
+    (d as any).toDate = function() { return this; };
+
+    out.push({
+      description: '',
+      note: '',
+      date: d,
+      duration: Math.random() * 10000
+    });
+  }
+  return out;
+};
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-  let fakeAfstore: FakeAfstore;
+  let fStore: FakeAfstore;
   let fakeAfAuth: FakeAfAuth;
+  let fakeDoc, fakeColl;
 
   beforeEach(async(() => {
-    fakeAfstore = new FakeAfstore();
+    fStore = new FakeAfstore();
     fakeAfAuth = new FakeAfAuth();
+    fakeDoc = new FakeAfstoreDoc();
+    fakeColl = new FakeAfstoreColl();
+
+    spyOn(fStore, 'doc')
+      .and.returnValue(fakeDoc);
+    spyOn(fakeDoc, 'collection')
+      .and.returnValue(fakeColl);
+
     TestBed.configureTestingModule({
       declarations: [
         HomeComponent,
@@ -25,7 +57,7 @@ describe('HomeComponent', () => {
       ],
       providers: [
         { provide: AngularFireAuth, useValue: fakeAfAuth },
-        { provide: AngularFirestore, useValue: fakeAfstore }
+        { provide: AngularFirestore, useValue: fStore }
       ]
     })
     .compileComponents();
@@ -40,4 +72,73 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get most recent entries from this month', () => {
+    const today = new Date();
+    const first = new Date();
+    first.setUTCDate(1);
+    first.setUTCHours(0, 0, 0);
+    fakeColl.collToReturn = makeEntriesBetween(first, today);
+    fakeAfAuth.user.next({});
+
+    const fs = TestBed.get(AngularFirestore);
+    expect(fs.doc).toHaveBeenCalled();
+    expect(fakeDoc.collection).toHaveBeenCalled();
+    expect(component.overviewData).toBeDefined();
+    expect(component.overviewData.length).not.toBe(0);
+  });
+
+  it('should get most recent entries from last month', () => {
+    const last = new Date();
+    last.setUTCDate(0);
+    last.setUTCHours(0, 0, 0);
+    const first = new Date(last);
+    first.setUTCDate(1);
+    fakeColl.collToReturn = makeEntriesBetween(first, last);
+    fakeAfAuth.user.next({});
+
+    const fs = TestBed.get(AngularFirestore);
+    expect(fs.doc).toHaveBeenCalled();
+    expect(fakeDoc.collection).toHaveBeenCalled();
+    expect(component.overviewData).toBeDefined();
+    expect(component.overviewData.length).not.toBe(0);
+  });
+
+  it('should stage most recent entry from this month', () => {
+    const today = new Date();
+    const first = new Date();
+    first.setUTCDate(1);
+    first.setUTCHours(0, 0, 0);
+    fakeColl.collToReturn = makeEntriesBetween(first, today);
+    fakeAfAuth.user.next({});
+    const mostRecentEntry = component.overviewData.reduce((prev, next) => {
+      if (!prev) {
+        return next;
+      }
+      return prev.date > next.date && !prev.isTodayMarker ? prev : next;
+    });
+
+    expect(component.stagedEntry.date).toBe(mostRecentEntry.date);
+  });
+
+
+  it('should stage most recent entry for previous month', () => {
+    const last = new Date();
+    last.setUTCDate(0);
+    last.setUTCHours(0, 0, 0);
+    const first = new Date(last);
+    first.setUTCDate(1);
+    fakeColl.collToReturn = makeEntriesBetween(first, last);
+    fakeAfAuth.user.next({});
+
+    const mostRecentEntry = component.overviewData.reduce((prev, next) => {
+      if (!prev) {
+        return next;
+      }
+      return prev.date > next.date && !prev.isTodayMarker ? prev : next;
+    });
+
+    expect(component.stagedEntry.date).toBe(mostRecentEntry.date);
+  });
+
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Entry } from '../models/entry.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { take, map, filter } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
+import { take, map, filter, switchMap } from 'rxjs/operators';
+import { Observable, Subject, of } from 'rxjs';
 import DateEntries from '../models/date-entries.interface';
 
 @Component({
@@ -28,7 +28,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       take(1)
     ).subscribe(u => {
       this.uid = u.uid;
-      this.getEntriesForMonth(u.uid, this.getFirstOfMonth())
+      this.getEntriesForMostRecentPopulatedMonth(u.uid, this.getFirstOfMonth())
         .subscribe((entries: Entry[]) => {
           this.overviewData = this.formatOverviewData(entries);
           this.onDateSelected(this.getMostRecentEntries(this.overviewData));
@@ -51,8 +51,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   private getMostRecentEntries(entries: DateEntries[]): DateEntries {
-    const todayDate = (new Date()).getDate();
-    return entries.filter(d => d.date.getDate() <= todayDate && !d.isTodayMarker)
+    const today = new Date();
+    return entries.filter(d => d.date <= today && !d.isTodayMarker)
       .sort((a, b) => a.date > b.date ? -1 : 1)[0];
   }
 
@@ -76,6 +76,31 @@ export class HomeComponent implements OnInit, OnDestroy {
         }))
       ) as Observable<Entry[]>;
   }
+
+  /**
+   * Wraps getEntriesForMonth, calling it for each preceding month
+   * until it finds one with entries (or maxes out its attempts).
+   *
+   * Recursive.
+   */
+  private getEntriesForMostRecentPopulatedMonth(uid: string, firstOfMonth: Date, attempt = 0): Observable<Entry[]> {
+    const max = 6;
+    return this.getEntriesForMonth(uid, firstOfMonth)
+      .pipe(
+        switchMap(e => {
+          if ((!e || !e.length) && attempt < max) {
+            firstOfMonth.setDate(0);
+            firstOfMonth.setDate(1);
+            attempt++;
+
+            // point of recursion
+            return this.getEntriesForMostRecentPopulatedMonth(uid, firstOfMonth, attempt);
+          }
+          return of(e);
+        })
+      );
+  }
+
 
   private formatOverviewData(entries: Entry[]): DateEntries[] {
     const newOverview: DateEntries[] = [];
