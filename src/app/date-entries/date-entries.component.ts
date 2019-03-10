f5a7051b5e58c670148c4d7dd460d748b562829f
Dave Riedstra <dave.riedstra@gmail.com>
2019-03-10 14:05:36 -0400

    filter out completely empty entries

--- src/app/date-entries/date-entries.component.ts
+++ src/app/date-entries/date-entries.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import DateEntries from '../models/date-entries.interface';
+import { Entry } from '../models/entry.model';
 
 @Component({
   selector: 'app-date-entries',
   templateUrl: './date-entries.component.html',
   styleUrls: ['./date-entries.component.scss']
 })
-export class DateEntriesComponent implements OnInit {
+export class DateEntriesComponent implements OnChanges {
   @Input()
   dateEntries: DateEntries;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['dateEntries'] && changes['dateEntries'].currentValue) {
+      this.dateEntries.entries = this.filterEntries(changes['dateEntries'].currentValue.entries);
+    }
   }
 
+  private filterEntries(entries: Entry[]): Entry[] {
+    return entries.filter(e => e.duration < 0 || !!e.description || !!e.note);
+  }
 }
